2a357c18cf8dd694b15ebf01e2bed27a2a6e187f
C. Reid <ctlnrd@gmail.com>
2017-05-04 00:48:51 -0400

    updated inventory appearance

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,6 @@
 var mysql      = require('mysql');
+var inquirer = require('inquirer');
+
 var connection = mysql.createConnection({
   host     : '127.0.0.1',
   port	   : '3306',
@@ -18,17 +20,31 @@ connection.query("SELECT * FROM products", function(error, result) {
 	if (error) {
 		throw error;
 	} else {
-		console.log("============================")
-		console.log("Welcome to Rifle Paper Co.!")
-		console.log("============================")
+		console.log("")
+		console.log("===============================================================")
+		console.log("                   Welcome to Rifle Paper Co.! ")
+		console.log("===============================================================")
+		console.log("")
+		console.log("----------------------- ITEMS FOR SALE ------------------------")
+		console.log("")
+
 		for (var i = 0; i < result.length; i++) {
 
-			console.log(result[i].department_name + " | " + result[i].product_name + " ((" + "$ " + result[i].price + ")) ")
+			console.log(result[i].item_id + ". " +result[i].department_name + " | " + result[i].product_name + " ((" + "$ " + result[i].price + "))")
+			console.log("")
 
 		}
-		console.log("============================")
+		console.log("===============================================================")
 		// console.log(result)
 	}
 });
+
+
+inquirer.prompt([/* Pass your questions in here */
+
+	]).then(function (answers) {
+    // Use user feedback for... whatever!! 
+});
+
 
 connection.end();
